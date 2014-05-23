c5b42c9f444d80816068fe7a973b1e9c8d76923d
Matteo Ferrando <matteo.ferrando2@gmail.com>
2014-05-23 16:12:37 -0430

    done with extras from chapter 5

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,1 @@
-# Retruns the full title on a per-page basis.
-def full_title(page_title)
-  base_title = "Sample Application"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
